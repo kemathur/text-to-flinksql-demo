d03ebef0603dbf23bdee75df960dbd65b52992d0
Keshav Mathur <kmathur@confluent.io>
2023-08-07 22:29:34 -0700

    make fe talk to backend

--- app-fe/app.py
+++ app-fe/app.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import requests
 
 def main():
     st.title("Hello Streamlit!")
     user_input = st.text_input("Type something:")
-    st.write(f"You typed: {user_input}")
+
+    if st.button("Send to API"):
+        response = call_api(user_input)
+        st.write(response.json()['message'])
+
+def call_api(input_text):
+    # Adjust the URL according to your setup
+    url = "http://python-app:5000/get-query/"
+    params = {"input-text": input_text}
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # This will raise an exception if the API request failed
+    return response
 
 if __name__ == "__main__":
     main()
+
