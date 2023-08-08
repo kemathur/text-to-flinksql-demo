56f159a44fd1427c0d63c0e8752c96a45f70337f
Keshav Mathur <kmathur@confluent.io>
2023-08-07 23:42:50 -0700

    add openai config

--- app/app.py
+++ app/app.py
@@ -1,14 +1,28 @@
 from flask import Flask, request, jsonify
 import re
+import logging
+import os
 
 app = Flask(__name__)
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+
+endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
+key = os.environ.get("AZURE_OPENAI_KEY")
+deployment = os.environ.get("AZURE_OPENAI_DEPLOYMENT")
+app.logger.info(f"endpoint: {endpoint}")
+app.logger.info(f"key: {key}")
+app.logger.info(f"deployment: {deployment}")
 
 @app.route('/get-query/', methods=['GET'])
 def get_query():
     input_text = request.args.get('input-text')
     if input_text:
         # Removing special characters using regex
-        cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', input_text) if input_text else ""
+        cleaned_text = input_text
+        # cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', input_text) if input_text else ""
+        app.logger.info(f"Cleaned Text: {cleaned_text}")
         return jsonify(message=f"Hello {cleaned_text}!")
     return jsonify(message="Hello World!")
 
