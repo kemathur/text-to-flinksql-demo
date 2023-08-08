0f1c4154a861793876027aeb74232b3d44ebe534
Keshav Mathur <kmathur@confluent.io>
2023-08-08 01:05:30 -0700

    integrate chatbot interface UI with backend

--- app-fe/app.py
+++ app-fe/app.py
@@ -1,18 +1,66 @@
+import time
 import streamlit as st
 import requests
+import json
 
 def main():
-    st.title("Hello Streamlit!")
-    user_input = st.text_input("Type something:")
+    # st.title("Hello Streamlit!")
+    # user_input = st.text_input("Type something:")
 
-    if st.button("Send to API"):
-        response = call_api(user_input)
-        st.write(response.json()['message'])
+    # if st.button("Send to API"):
+    #     response = call_api(user_input)
+    #     st.write(response.json()['message'])
 
-def call_api(input_text):
+    st.title("Chat interface")
+
+    # Initialize chat history from previous session(s)
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What's up?"):
+        
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            
+            # response = call_api(user_input)
+            # st.markdown(response.json()['message'])
+            st.markdown(prompt)
+
+        # Add user message to chat component
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            
+            # Call API server
+            response = call_api(prompt, st.session_state.messages)
+            assistant_response = response.json()['message']
+            
+            # Simulate stream of response with milliseconds delay
+            for chunk in assistant_response.split():
+                full_response += chunk + " "
+                time.sleep(0.05)
+                # Add a blinking cursor to simulate typing
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+def call_api(input_text, messages):
     # Adjust the URL according to your setup
     url = "http://python-app:5000/get-query/"
-    params = {"input-text": input_text}
+    params = {
+        "input-text": input_text,
+        "messages": json.dumps(messages)  # Convert list of dictionaries to a JSON string
+    }
     response = requests.get(url, params=params)
     response.raise_for_status()  # This will raise an exception if the API request failed
     return response
--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import json
+import openai
 import re
 import logging
 import os
@@ -14,6 +16,19 @@ deployment = os.environ.get("AZURE_OPENAI_DEPLOYMENT")
 app.logger.info(f"endpoint: {endpoint}")
 app.logger.info(f"key: {key}")
 app.logger.info(f"deployment: {deployment}")
+"""
+Code References:
+https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
+https://learn.microsoft.com/en-us/azure/ai-services/openai/chatgpt-quickstart?tabs=command-line&pivots=programming-language-python
+"""
+
+################
+
+# Set OpenAI API key from Streamlit secrets
+openai.api_base = endpoint # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
+openai.api_key = key
+openai.api_type = 'azure'
+openai.api_version = '2023-05-15' # this may change in the future
 
 @app.route('/get-query/', methods=['GET'])
 def get_query():
@@ -23,7 +38,26 @@ def get_query():
         cleaned_text = input_text
         # cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', input_text) if input_text else ""
         app.logger.info(f"Cleaned Text: {cleaned_text}")
-        return jsonify(message=f"Hello {cleaned_text}!")
+        
+        # Deserialize the JSON string to a list of dictionaries
+        messages_str = request.args.get('messages', "[]")
+        try:
+            messages = json.loads(messages_str)
+        except json.JSONDecodeError:
+            return jsonify(error="Invalid messages format"), 400
+        response = openai.ChatCompletion.create(
+                engine=deployment, 
+                messages = 
+                [ \
+                    {"role": message["role"], "content": message["content"]}
+                    for message in messages
+                ] # Feed in past messages (both user and chatbot generated)
+                + [ \
+                    {"role": "user", "content": cleaned_text}
+                ]
+            )
+        assistant_response = response['choices'][0]['message']["content"].replace('\n', '').replace(' .', '.').strip()
+        return jsonify(message=f"{assistant_response}!")
     return jsonify(message="Hello World!")
 
 if __name__ == '__main__':
